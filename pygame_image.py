bbc537d96c174aa3e320e6fb4c422d99041e1d23
R_k <c0b230407a@edu.teu.ac.jp>
2024-04-16 18:24:24 +0900

    演習1,2

--- pygame_image.py
+++ pygame_image.py
@@ -22,14 +22,18 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst=pg.key.get_pressed()
+        x=-1
+        y=0
         if key_lst [pg.K_UP]:
-            kk_rct.move_ip([0,-1])
+            y-=1
         if key_lst [pg.K_DOWN]:
-            kk_rct.move_ip([0,+1])
+            y+=1
         if key_lst [pg.K_LEFT]:
-            kk_rct.move_ip([-1,0])
+            x-=1
         if key_lst [pg.K_RIGHT]:
-            kk_rct.move_ip([+1,0])
+            x+=2
+        kk_rct.move_ip((x,y))
+
         x=tmr%800
         x=tmr%3200
 
