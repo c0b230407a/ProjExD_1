b505a556c01ec8d5921b87d21ccf852973828bd3
R_k <c0b230407a@edu.teu.ac.jp>
2024-04-16 18:16:49 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -18,6 +18,8 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        x=tmr%800
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kk_img, [300, 200]) 
         pg.display.update()
         tmr += 1        
