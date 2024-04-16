10c30a69806e72a7b14255e16f68ddc9dcfe4a4b
R_k <c0b230407a@edu.teu.ac.jp>
2024-04-16 18:14:48 +0900

    練習4

--- pygame_image.py
+++ pygame_image.py
@@ -10,41 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2=pg.transform.flip(bg_img,True,False)
-    kk_img =pg.image.load("fig/3.Png")
-    kk_img=pg.transform.flip(kk_img,True,False)
-    kk_rct=kk_img.get_rect()
-    kk_rct.center= 300,200
+    kk_img = pg.image.load("fig/3.png")  
+    kk_img = pg.transform.flip(kk_img, True, False)  
     tmr = 0
-    enn=pg.Surface((20,20))
-    pg.draw.circle(enn,(255,0,0),(10,10),10)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst=pg.key.get_pressed()
-        if key_lst [pg.K_UP]:
-            kk_rct.move_ip([0,-1])
-        if key_lst [pg.K_DOWN]:
-            kk_rct.move_ip([0,+1])
-        if key_lst [pg.K_LEFT]:
-            kk_rct.move_ip([-1,0])
-        if key_lst [pg.K_RIGHT]:
-            kk_rct.move_ip([+1,0])
-        x=tmr%800
-        x=tmr%3200
 
-        screen.blit(bg_img,[-x,0])
-        screen.blit(bg_img2,[-x+1600,0])
-        screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg_img2,[-x+4800,0])
-        screen.blit(kk_img,kk_rct)
-        
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300, 200]) 
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
-
-def new_func(event):
-    if event.type == pg.QUIT: return
+        clock.tick(10)
 
 
 if __name__ == "__main__":
